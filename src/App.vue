6cd97a8d60bbf62e30b7c03fd0ec978a591d0248
Mien <2502mien@gmail.com>
2022-04-01 14:48:25 +0700

    add

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,48 @@
 <script>
-
-
 export default {
-  name: 'App',
+  name: "App",
+
   data() {
     return {
-      previous: null,
-      display: 0,
-      operator: null,
-      operatorClicked: false
-    };
+      formula: '',
+      result: null,
+    }
   },
   methods: {
-    clear() {
-      this.display = 0;
+    getNumber(value) {
+      this.formula = value;
     },
-
-    append(number) {
-      if (this.operatorClicked) {
-        this.display = '';
-        this.operatorClicked = false;
-      }
-      this.display = this.display === 0 ? (this.display = number) : '' + this.display + number;
-    },
-    divide() {
-      this.operator = (a, b) => a / b;
-      this.previous = this.display;
-      this.operatorClicked = true;
-    },
-    multiply() {
-      this.operator = (a, b) => a * b;
-      this.previous = this.display;
-      this.operatorClicked = true;
-    },
-    subtract() {
-      this.operator = (a, b) => a - b;
-      this.previous = this.display;
-      this.operatorClicked = true;
-    },
-    add() {
-      this.operator = (a, b) => a + b;
-      this.previous = this.display;
-      this.operatorClicked = true;
-    },
-    equal() {
-      this.display = this.operator(Number(this.previous), Number(this.display));
-      this.previous = null;
-      this.operatorClicked = true;
-    }
   }
-
 }
 </script>
+
 
 <template>
   <div id="app">
     <div class="calc-body">
-      <div class="display">
-        {{ display }}
+      <div class="calculator-display">
+        <div class="calculator-formula" v-cloak>{{ formula }}</div>
+        <div class="calculator-result" v-cloak>{{ result }}</div>
       </div>
       <div/>
-      <button @click='append(7)' class="number number-7">7</button>
-      <button @click='append(8)' class="number number-8">8</button>
-      <button @click='append(9)' class="number number-9">9</button>
-      <button @click='divide' class="number number-div">/</button>
-      <button @click='append(4)' class="number number-4">4</button>
-      <button @click='append(5)' class="number number-5">5</button>
-      <button @click='append(6)' class="number number-6">6</button>
-      <button @click='multiply' class="number number-mul">*</button>
-      <button @click='append(1)' class="number number-1">1</button>
-      <button @click='append(2)' class="number number-2">2</button>
-      <button @click='append(3)' class="number number-3">3</button>
-      <button @click='add' class="number number-add">+</button>
-      <button @click='append(0)' class="number number-0">0</button>
-      <button @click='clear' class="number number-clear">Clear</button>
-      <button @click='equal' class="number number-equal">=</button>
-      <button @click='subtract' class="number number-sub">-</button>
+      <div class="calc-inputs">
+        <button class="btn_square alt" aria-label="Clear" @click="clearAll">CE</button>
+        <button class="btn_square" aria-label="Zero" @click="getNumber(0)">0</button>
+        <button class="btn_square" aria-label="One" @click="getNumber(1)">1</button>
+        <button class="btn_square" aria-label="Two" @click="getNumber(2)">2</button>
+        <button class="btn_square" aria-label="Three" @click="getNumber(3)">3</button>
+        <button class="btn_square" aria-label="Four" @click="getNumber(4)">4</button>
+        <button class="btn_square" aria-label="Five" @click="getNumber(5)">5</button>
+        <button class="btn_square" aria-label="Six" @click="getNumber(6)">6</button>
+        <button class="btn_square" aria-label="Seven" @click="getNumber(7)">7</button>
+        <button class="btn_square" aria-label="Eight" @click="getNumber(8)">8</button>
+        <button class="btn_square" aria-label="Nine" @click="getNumber(9)">9</button>
+        <button class="btn_square alt" aria-label="add" @click="add('+')">+</button>
+        <button class="btn_square alt" aria-label="subtract" @click="subtract('-')">-</button>
+        <button class="btn_square alt" aria-label="multiply" @click="multiply('x')">*</button>
+        <button class="btn_square alt" aria-label="division" @click="division('%')">%</button>
+        <button class="btn_square alt" aria-label="equals" @click="equals()">=</button>
+      </div>
     </div>
   </div>
 </template>
@@ -95,7 +62,7 @@ export default {
   align-content: start;
 }
 
-.display {
+.calculator-display {
   width: 500px;
   background: pink;
   height: 40px;
@@ -104,7 +71,7 @@ export default {
   align-items: center;
 }
 
-.number {
+.btn_square {
   margin: 10px 10px 10px 10px;
   width: 100px;
   height: 50px;
