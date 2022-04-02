0a0aa6277628f61a0f6c214e9da2467f203d69b7
Mien <2502mien@gmail.com>
2022-04-03 04:34:14 +0700

    add

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,76 @@
 <script>
-export  default {
+export default {
   name: 'App',
   data() {
     return {
-      number: 1,
-      message:"hello",
-      users: {
-        name:'mien',
-        age:20
-      }
+      products: [],
+      carts: [],
+      search: '',
+      active: false,
     }
   },
   methods: {
-    Increment(){
-      return this.number++;
+    getData() {
+      fetch(`https://6246a3c3e3450d61b000fdf0.mockapi.io/products?search=${this.search}`).then(response => response.json()).then((data) => {
+        this.products.push(data)
+      })
+      this.products.splice(0, 1)
+    },
+    onSubmit() {
+      this.getData();
+      this.search = '';
+    },
+    addToCart(id) {
+      this.carts.push(this.products.flat().find(product => product.id === id));
+      localStorage.setItem('listCart', JSON.stringify(this.carts));
+      alert('sản phẩm đã được thêm vào giỏ hàng !');
+    },
+    showCart() {
+      this.active = !this.active;
+      this.products = [];
+      if (localStorage.getItem('listCart')) {
+        this.carts = JSON.parse(localStorage.getItem('listCart'));
+      }
+    },
+    showHome() {
+      this.active = !this.active;
+      this.getData();
     }
+  },
+  mounted() {
+    this.getData();
   },
 }
 </script>
 
 <template>
   <div id="app">
-    {{name}}
-    <div>{{message | to-UpperCase}}</div>
-    <button @click="Increment">button</button>
+
+    <div class="menu">
+      <div>
+        <form action="" @submit.prevent="onSubmit()">
+          <input type="text" v-model="search" placeholder="Search" class="search">
+        </form>
+      </div>
+      <div class="cart" @click="showCart()">cart</div>
+      <div class="cart" @click="showHome()">home</div>
+    </div>
+
+    <div v-for="(cart, index) in carts" :key="index" v-show="active">
+      <div>{{ cart.name }}</div>
+      <img :src="cart.thumb" alt="">
+    </div>
+
+    <ul v-for="(item, index) in products.flat()" :key="index">
+      <div> Name :{{ item.name }}</div>
+      <div> Price :{{ item.price }}</div>
+      <div> Description :{{ item.description }}</div>
+      <div>
+        <button @click="addToCart(item.id)">add to cart</button>
+      </div>
+      <img :src="item.thumb" alt="">
+    </ul>
+
   </div>
 
 </template>
@@ -31,5 +78,30 @@ export  default {
 <style>
 #app {
   text-align: center;
+}
+
+.search {
+  width: 500px;
+  height: 30px;
+  border-radius: 10px;
+}
+
+.menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.cart {
+  width: 66px;
+  height: 36px;
+  background: red;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  font-size: 23px;
+  cursor: pointer;
 }
 </style>
